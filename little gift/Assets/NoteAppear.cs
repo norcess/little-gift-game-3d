81a354482b0c424afef2caa4f9c35a148a9b6e87
norcess <hwang2022@elam.co.uk>
2022-12-05 14:41:28 +0000

    help me(fish and sound)

--- little gift/Assets/NoteAppear.cs
+++ little gift/Assets/NoteAppear.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class NoteAppear : MonoBehaviour
 {
     public Image _noteImage;
+    public AudioClip lettersound;
 
     void OnTriggerEnter(Collider other) 
     {
         {
+            AudioSource.PlayClipAtPoint(lettersound, transform.position);
             if (other.CompareTag("Player"))
             {
                 _noteImage.enabled = true;
